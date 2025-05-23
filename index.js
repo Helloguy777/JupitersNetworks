daebb8747447f41dc5371ca75ae61299fc1ca572
Helloguy777 <askhatbaev@outlook.com>
2025-05-23 18:39:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
-const { hostname } = require("os");
-const path = require("path");
-const express = require("express");
-const wisp = require("wisp-server-node");
-const cors = require("cors");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+import fs from "fs";
+import http from "http";
+import https from "https";
+import tls from "tls";
+import path from "path";
+import express from "express";
+import wisp from "wisp-server-node";
+import cors from "cors";
+import { fileURLToPath } from "url";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const pubDir = path.join(__dirname, "public");
-const { uvPath } = require("@titaniumnetwork-dev/ultraviolet");
-const { epoxyPath } = require("@mercuryworkshop/epoxy-transport");
-const { libcurlPath } = require("@mercuryworkshop/libcurl-transport");
-const { baremuxPath } = require("@mercuryworkshop/bare-mux/node");
 
 const app = express();
 
+app.use(cors());
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
@@ -27,7 +31,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(pubDir));
-
 app.get("/uv/uv.config.js", (req, res) => {
 	res.sendFile(path.join(pubDir, "uv/uv.config.js"));
 });
@@ -42,34 +45,48 @@ app.use((req, res) => {
 });
 
 const httpServer = http.createServer(app);
+
+const sslContexts = {
+	"study.factwiki.me": tls.createSecureContext({
+		key: fs.readFileSync("/etc/letsencrypt/live/study.factwiki.me/privkey.pem"),
+		cert: fs.readFileSync("/etc/letsencrypt/live/study.factwiki.me/fullchain.pem")
+	}),
+	"jupitersnet.miceforlife.com": tls.createSecureContext({
+		key: fs.readFileSync("/etc/letsencrypt/live/jupitersnet.miceforlife.com/privkey.pem"),
+		cert: fs.readFileSync("/etc/letsencrypt/live/jupitersnet.miceforlife.com/fullchain.pem")
+	}),
+	"jupiter.bsfa.info": tls.createSecureContext({
+		key: fs.readFileSync("/etc/letsencrypt/live/jupiter.bsfa.info/privkey.pem"),
+		cert: fs.readFileSync("/etc/letsencrypt/live/jupiter.bsfa.info/fullchain.pem")
+	}),
+	"jupitersnet.leasingindia.com": tls.createSecureContext({
+		key: fs.readFileSync("/etc/letsencrypt/live/jupitersnet.leasingindia.com/privkey.pem"),
+		cert: fs.readFileSync("/etc/letsencrypt/live/jupitersnet.leasingindia.com/fullchain.pem")
+	})
+};
+
 const httpsServer = https.createServer({
-	key: fs.readFileSync("key.pem"),
-	cert: fs.readFileSync("cert.pem")
+	SNICallback: (servername, cb) => {
+		if (sslContexts[servername]) cb(null, sslContexts[servername]);
+		else cb(new Error("No SSL cert for " + servername));
+	}
 }, app);
 
 httpServer.on("upgrade", (req, socket, head) => {
-	if (req.url.endsWith("/wisp/")) {
-		wisp.routeRequest(req, socket, head);
-	} else {
-		socket.end();
-	}
+	if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
+	else socket.end();
 });
 
 httpsServer.on("upgrade", (req, socket, head) => {
-	if (req.url.endsWith("/wisp/")) {
-		wisp.routeRequest(req, socket, head);
-	} else {
-		socket.end();
-	}
+	if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
+	else socket.end();
 });
 
 httpServer.listen(80, "0.0.0.0");
 httpsServer.listen(443, "0.0.0.0");
 
-console.log(`
-| JupitersNetworks 1.0 |
+console.log(`| JupitersNetworks 1.0 |
 
 Running on:
 	http://localhost:80
-	https://localhost:443
-`);
+	https://localhost:443`);
